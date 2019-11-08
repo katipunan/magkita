d972fda75fa04b2833b0d360b7cc6e302b7d0c29
mikong <4162+mikong@users.noreply.github.com>
2019-11-08 23:46:08 +0800

    test: Refactor user test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,19 +4,26 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   setup do
-    @user = User.new(email: 'user1@example.com')
+    @user = User.new(
+      email: 'user1@example.com',
+      password: 'password123',
+      password_confirmation: 'password123'
+    )
   end
 
   test 'must have long password' do
-    @user.password = 'short12'
+    short_password = 'short12'
+    @user.password = short_password
+    @user.password_confirmation = short_password
     refute @user.valid?, 'Saved user with a short password'
-    assert_not_nil @user.errors[:password], 'No validation error for password'
+    assert_not @user.errors[:password].blank?,
+      'No validation error for password'
   end
 
   test 'must have password confirmation' do
-    @user.password = 'password123'
+    @user.password_confirmation = nil
     refute @user.valid?, 'Saved user with no password confirmation'
-    assert_not_nil @user.errors[:password_confirmation],
+    assert_not @user.errors[:password_confirmation].blank?,
       'No validation error for password confirmation'
   end
 
